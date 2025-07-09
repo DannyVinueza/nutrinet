1a7eb40d174509a0e832ca7d92ca2f179f121896
DannyVinueza <danny.vinueza@epn.edu.ec>
2025-07-08 21:43:08 -0500

    Initial commit

---
+++ src/main/java/com/appweb/nutrinet/NutrinetApplication.java
@@ -0,0 +1,13 @@
+package com.appweb.nutrinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrinetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appweb/nutrinet/NutrinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appweb.nutrinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrinetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
